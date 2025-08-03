007258a43db3a9b9f4321113737afd82fdc774c9
vedantbhole <vabhole4@gmail.com>
2025-08-03 17:01:23 +0530

    initial commit: project setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import {createRoot} from "react-dom/client";
+
+export const App = () => {
+    return (
+        <div><h1 class="text-3xl font-bold underline text-red-500">
+            Hello world!
+        </h1></div>
+    )
+}
+export default App
